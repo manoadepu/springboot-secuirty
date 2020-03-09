8421ed07b8de98d2d790cdad9aadd47b4fd09e0d
mano.adepu@gmail.com <saibaba121>
2020-03-08 22:30:45 -0500

    project setup

---
+++ src/test/java/com/manoharadepu/Security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manoharadepu.Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
